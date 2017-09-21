0b05f0fbaef1dbeca0fe1bb99989ae2a862cd59f
Viking & Robot <vikingandrobot@gmail.com>
2017-09-21 12:02:47 +0200

    First implementation fo slaves

---
+++ Slave/src/Main.java
@@ -0,0 +1,26 @@
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import slave.Slave;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author mathieu
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Slave slave = new Slave("228.5.6.7");
+            slave.run();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
---
+++ Slave/src/slave/Slave.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slave;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author mathieu
+ */
+public class Slave implements Runnable {
+    
+    private MulticastSocket socket;
+    private InetAddress group;
+    
+    public Slave(String IPGroup) throws IOException {
+        System.setProperty("java.net.preferIPv4Stack", "true");
+        System.out.println("Creating socket...");
+        socket = new MulticastSocket(12000);
+        System.out.println("Joining group " + IPGroup);
+        group = InetAddress.getByName(IPGroup);
+        socket.joinGroup(group);
+        System.out.println("Set up completed");
+    }
+
+    @Override
+    public void run() {
+        
+        try {
+            DatagramPacket packet;
+            while (true) {
+                try {
+                    byte[] buf = new byte[256];
+                    packet = new DatagramPacket(buf, buf.length);
+                    System.out.println("Waiting for UDP packet");
+                    socket.receive(packet);
+                    
+                    String received = new String(packet.getData());
+                    System.out.println("Data received: " + received);
+                } catch (IOException ex) {
+                    Logger.getLogger(Slave.class.getName()).log(Level.SEVERE, null, ex);
+                    break;
+                }
+            }
+            
+            socket.leaveGroup(group);
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Slave.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+    }
+    
+}
