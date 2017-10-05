c0ab0089212a8cdd1de70e2db0391fb659cbd186
ripwin <sathiya.kirushnapillai@heig-vd.ch>
2017-10-05 16:52:38 +0200

    Add SYNC and FOLLOW_UP packet

--- Master/src/main/java/ch/heigvd/prr/master/Master.java
+++ Master/src/main/java/ch/heigvd/prr/master/Master.java
@@ -1,70 +1,15 @@
 package ch.heigvd.prr.master;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.MulticastSocket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Master implements Runnable {
    
-   private final int interval;
-   private final String address;
-   private final int port;
-   
-   private boolean running;
+   private Synchronization sync;
    
    public Master(int interval, String address, int port) {
-      this.interval = interval;
-      this.address = address;
-      this.port = port;
+      this.sync = new Synchronization(address, port, interval);
    }
 
    @Override
    public void run() {
-      
-      running = true;
-      
-      try (
-         MulticastSocket socket = new MulticastSocket(12000)) 
-      {
-         
-
-         byte[] tampon = new byte[256];
-         tampon = "Allooooooo".getBytes();
-         
-         
-         InetAddress group = InetAddress.getByName(address);
-         
-         
-         // Paquet à envoyer
-         DatagramPacket packet = new DatagramPacket(
-            tampon, 
-            tampon.length, 
-            group, 
-            port
-         );
-
-         while(running) {
-            
-            socket.send(packet);
-            System.out.println("Envoie du message" + packet.getData().length);
-            
-            
-            try {
-               Thread.sleep(interval);
-            } catch (InterruptedException ex) {
-               Logger.getLogger(Master.class.getName()).log(Level.SEVERE, null, ex);
-            }
-         }
-         
-      } catch (IOException ex) {
-         Logger.getLogger(Master.class.getName()).log(Level.SEVERE, null, ex);
-      }
+      sync.run();
    }
-   
-
-
 }
---
+++ Master/src/main/java/ch/heigvd/prr/master/Synchronization.java
@@ -0,0 +1,104 @@
+package ch.heigvd.prr.master;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import ch.heigvd.prr.common.Protocol;
+
+public class Synchronization implements Runnable {
+   
+   private final String address;
+   private final int port;
+   private final int interval;
+   
+   private int id;
+   
+   private boolean running;
+   
+   public Synchronization(String address, int port, int interval) {
+      this.address = address;
+      this.port = port;
+      this.interval = interval;
+      
+      this.id = 0;
+   }
+   
+   @Override
+   public void run() {
+      
+      running = true;
+      
+      try (
+         MulticastSocket socket = new MulticastSocket()) 
+      {
+         InetAddress group = InetAddress.getByName(address);
+         
+         
+         while(running) {
+            
+            // Incrémente l'identifiant
+            this.id++;
+            long time;
+         
+            // SYNC
+            {
+               // Message SYNC
+               ByteBuffer buffer = ByteBuffer.allocate(8);
+               buffer.put(Protocol.getByte(Protocol.Code.SYNC));
+               buffer.putInt(id);
+
+               byte[] data = buffer.array();
+
+               // Création du paquet
+               DatagramPacket packet = new DatagramPacket(
+                  data, 
+                  data.length, 
+                  group, 
+                  port
+               );
+
+               socket.send(packet);
+               time = System.currentTimeMillis();
+               System.out.println("Envoie du SYNC");
+            }
+            
+            // FOLLOW_UP
+            {
+               // Message FOLLOW_UP
+               ByteBuffer buffer = ByteBuffer.allocate(32);
+               buffer.put(Protocol.getByte(Protocol.Code.FOLLOW_UP));
+               buffer.putLong(time);
+               buffer.putInt(id);
+
+               byte[] data = buffer.array();
+
+               // Création du paquet
+               DatagramPacket packet = new DatagramPacket(
+                  data, 
+                  data.length, 
+                  group, 
+                  port
+               );
+
+               socket.send(packet);
+               System.out.println("Envoie du FOLLOW_UP");
+            }
+            
+            // Intervale entre les envoies
+            try {
+               Thread.sleep(interval);
+            } catch (InterruptedException ex) {
+               Logger.getLogger(Master.class.getName()).log(Level.SEVERE, null, ex);
+            }
+         }
+         
+      } catch (IOException ex) {
+         Logger.getLogger(Master.class.getName()).log(Level.SEVERE, null, ex);
+      }
+   }
+}
