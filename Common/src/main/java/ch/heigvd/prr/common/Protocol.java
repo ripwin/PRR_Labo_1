aea59942122564e4979e613e0b3ed28db83ff2e6
Viking & Robot <vikingandrobot@gmail.com>
2017-11-02 09:30:17 +0100

    format.

--- Common/src/main/java/ch/heigvd/prr/common/Protocol.java
+++ Common/src/main/java/ch/heigvd/prr/common/Protocol.java
@@ -13,64 +13,64 @@ import java.net.UnknownHostException;
  */
 public class Protocol {
 
-    /**
-     * Enum to identify the PTP messages
-     */
-    public enum Code {
-        SYNC, FOLLOW_UP, DELAY_REQUEST, DELAY_RESPONSE
-    };
+   /**
+    * Enum to identify the PTP messages
+    */
+   public enum Code {
+      SYNC, FOLLOW_UP, DELAY_REQUEST, DELAY_RESPONSE
+   };
 
-    // The used multicast port 
-    public static final int MULTICAST_PORT = 12000;
+   // The used multicast port 
+   public static final int MULTICAST_PORT = 12000;
 
-    // The used port to communicate DELAY_REQUESTS
-    public static final int DELAY_COMMUNICATION_PORT = 13000;
+   // The used port to communicate DELAY_REQUESTS
+   public static final int DELAY_COMMUNICATION_PORT = 13000;
 
-    //  SYNC waiting interval
-    public static final int INTERVAL_SYNC = 4000;
+   //  SYNC waiting interval
+   public static final int INTERVAL_SYNC = 4000;
 
-    // Min of waiting range for DELAY_REQUEST messages
-    public static final int INTERVAL_DELAY_MIN = INTERVAL_SYNC * 4;
+   // Min of waiting range for DELAY_REQUEST messages
+   public static final int INTERVAL_DELAY_MIN = INTERVAL_SYNC * 4;
 
-    // MAX of waiting range for DELAY_REQUEST messages
-    public static final int INTERVAL_DELAY_MAX = INTERVAL_SYNC * 60;
+   // MAX of waiting range for DELAY_REQUEST messages
+   public static final int INTERVAL_DELAY_MAX = INTERVAL_SYNC * 60;
 
-    /**
-     * Convert Code enum value to byte.
-     *
-     * @param code the Code enum code
-     * @return the corresponding byte value
-     */
-    public static byte getByte(Protocol.Code code) {
-        return (byte) code.ordinal();
-    }
+   /**
+    * Convert Code enum value to byte.
+    *
+    * @param code the Code enum code
+    * @return the corresponding byte value
+    */
+   public static byte getByte(Protocol.Code code) {
+      return (byte) code.ordinal();
+   }
 
-    /**
-     * Convert byte value to corresponding Code enum value
-     *
-     * @param byteCode the byte value
-     * @return the Code enum value
-     */
-    public static Code getEnum(byte byteCode) {
-        if (byteCode >= Code.values().length) {
-            throw new IllegalArgumentException();
-        }
+   /**
+    * Convert byte value to corresponding Code enum value
+    *
+    * @param byteCode the byte value
+    * @return the Code enum value
+    */
+   public static Code getEnum(byte byteCode) {
+      if (byteCode >= Code.values().length) {
+         throw new IllegalArgumentException();
+      }
 
-        return Code.values()[byteCode];
-    }
+      return Code.values()[byteCode];
+   }
 
-    /**
-     * Get the multicast address. This method forces the use of IPV4 on MacOS
-     * Source: https://stackoverflow.com/questions/18747134/
-     * getting-cant-assign-requested-address-java-net-socketexception-using-ehcache
-     * 01.11.2017
-     *
-     * @return the multicast address.
-     * @throws UnknownHostException
-     */
-    public static InetAddress getMulticastAddress() throws UnknownHostException {
-        // To prevent MacOS using ipv6 addresses by default
-        System.setProperty("java.net.preferIPv4Stack", "true");
-        return InetAddress.getByName("239.0.0.1");
-    }
+   /**
+    * Get the multicast address. This method forces the use of IPV4 on MacOS
+    * Source: https://stackoverflow.com/questions/18747134/
+    * getting-cant-assign-requested-address-java-net-socketexception-using-ehcache
+    * 01.11.2017
+    *
+    * @return the multicast address.
+    * @throws UnknownHostException
+    */
+   public static InetAddress getMulticastAddress() throws UnknownHostException {
+      // To prevent MacOS using ipv6 addresses by default
+      System.setProperty("java.net.preferIPv4Stack", "true");
+      return InetAddress.getByName("239.0.0.1");
+   }
 }
